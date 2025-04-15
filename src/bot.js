eb9226b5b0db828e6ea76e73621058e79a38f988
Vasilew69 <115429188+Vasilew69@users.noreply.github.com>
2025-04-15 13:13:24 +0300

    updates

--- src/bot.js
+++ src/bot.js
@@ -12,6 +12,7 @@ const { Player } = require('discord-player');
 const { DefaultExtractors } = require('@discord-player/extractor');
 const { YoutubeiExtractor } = require('discord-player-youtubei');
 
+
 dotenv.config({ path: './configs/.env' });
 const token = process.env['token'];
 
